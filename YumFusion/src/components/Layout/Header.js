9bcaef2900d92dd4c63ce116ff912d6282f1edcf
minajchy <minajchy.123@gmail.com>
2024-03-25 00:48:18 +0530

    CartOpenClose

--- YumFusion/src/components/Layout/Header.js
+++ YumFusion/src/components/Layout/Header.js
@@ -1,19 +1,21 @@
-import {Fragment} from 'react';
+import { Fragment } from "react";
 
-import mealsImege from '../../assets/meals.jpg';
-import classes from './Header.module.css';
-import HeaderCartButton from './HeaderCartButton';
+import mealsImege from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCartButton></HeaderCartButton>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImege} alt="A table full of delicious food!"></img>
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImege} alt="A table full of delicious food!"></img>
+      </div>
     </Fragment>
+  );
 };
 
 export default Header;
